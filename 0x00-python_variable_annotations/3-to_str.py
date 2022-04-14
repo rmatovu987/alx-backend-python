c1a1c595fd1bebc7f168e83ef3f3263080f16b0c
rmatovu987 <rmatovu987@gmail.com>
2022-04-14 10:23:52 +0300

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function takes float and returns string representation"""
+
+
+def to_str(n: float) -> str:
+    """Function takes float and returns string representation"""
+    return repr(n)
