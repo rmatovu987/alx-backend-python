8ce1d168bc6deb797f03374d32c5d5e71d490b9b
rmatovu987 <rmatovu987@gmail.com>
2022-04-14 10:07:22 +0300

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns the floor of float"""
+import math
+
+
+def floor(n: float) -> float:
+    """Function to return floor of a float"""
+    return math.floor(n)
