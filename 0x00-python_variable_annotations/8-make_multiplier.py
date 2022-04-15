1177386105c7ab361a65daad6149616e0f39cab5
rmatovu987 <rmatovu987@gmail.com>
2022-04-15 07:47:56 +0300

    Task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+type-annotated function make_multiplier that takes a float multiplier as
+argument and returns a function that multiplies a float by multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return function that multiplies arg with function"""
+    def multFunc(func: float) -> float:
+        """return float of multiplied func and arg"""
+        return multiplier * func
+    return multFunc
