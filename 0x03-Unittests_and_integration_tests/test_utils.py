21640c3798b70c0f416754c9e4f1560bbbf0687d
rmatovu987 <rmatovu987@gmail.com>
2022-05-09 14:13:41 +0300

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,8 @@
 """In this task you will write
 the first unit test for utils.access_nested_map"""
 import unittest
+from unittest.mock import patch
+
 from utils import *
 
 from parameterized import parameterized
@@ -10,6 +12,7 @@ from parameterized import parameterized
 class TestAccessNestedMap(unittest.TestCase):
     """tests the  utils.access_nested_map
     """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -35,3 +38,22 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual((path[len(path) - 1]), context.exception.args[0])
+
+
+class TestGetJson(unittest.TestCase):
+    """ Tests that the function utils.get_json returns expected result """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ tests utils.get_json returns the expected result """
+        mock_get.return_value = test_payload
+        res = get_json(test_url)
+        self.assertEqual(res, test_payload)
+
+
+if __name__ == '__main__':
+    unittest.main()
